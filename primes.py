98dbb614eaa4a3fddee1d4ee493aed1a332500ee
Jake Rawal (k22001853) <jake.rawal3@gmail.com>
2023-09-28 18:36:00 +0100

    Complted Code

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f"number_of_primes={number_of_primes} should have been positive ")
     list = []
+    current = 1
+    while len(list) < number_of_primes:
+        if testIfPrime(current):
+            list.append(current)
+        current+=1
     return list
+
+def testIfPrime(number):
+    factors = 0
+    for i in range(1,number+1):
+        if number % i == 0:
+            factors += 1
+    return factors == 2
